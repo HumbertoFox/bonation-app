8e5b9c74a086fb5f32f6127a6332b8d4899a030f
Humberto Ribeiro <betofoxmal@gmail.com>
2024-08-24 17:36:21 -0300

    update app default remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import Menu from './components/menu/menu';
-import styles from './page.module.css';
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Menu />
-      <div className={styles.divmain}>
+    <main>
 
-      </div>
     </main>
   );
 };
