efcb92b63e05cbd9dea877578cf2c160482b7409
Humberto Ribeiro <betofoxmal@gmail.com>
2024-08-26 01:10:05 -0300

    upgrade menu

--- src/components/forms/form.tsx
+++ src/components/forms/form.tsx
@@ -15,37 +15,41 @@ export default function FormFull({ title, value }: TitleValue) {
     const [ispasswordchecked, setIspasswordchecked] = useState(false);
     const handlePassword = () => setIspassword(!ispassword);
     const handlePasswordChecked = () => setIspasswordchecked(!ispasswordchecked);
-    
+
     return (
         <form>
             {title === 'Cadastrar Veículo' && <div className='flex flex-col gap-[0.313rem]'>
+                <input type='text' id='modelo' placeholder='Modelo' className='rounded py-0.5' />
                 <input type='text' id='chassi' placeholder='Chassi' className='rounded py-0.5' />
                 <input type='text' id='plate' placeholder='Placa' className='rounded py-0.5' />
                 <input type='number' id='km' placeholder='Km' className='rounded py-0.5' />
             </div>
             }
             {title !== 'Cadastrar Veículo' && <fieldset className='flex flex-col gap-[0.313rem]'>
-                <input type='text' id='name' placeholder={title === 'Cadastrar Veículo' ? 'Modelo' : 'Nome'} className='rounded py-0.5' />
+                {title !== 'Entrar' && <input type='text' id='name' placeholder='Nome' className='rounded py-0.5' />}
                 <input type='text' placeholder='CPF' className='rounded py-0.5' />
                 {title === 'Cadastrar Motorista' && <input type='number' id='cnh' placeholder='CNH' className='rounded py-0.5' />}
-                <input type='number' id='zipcode' placeholder='CEP' className='rounded py-0.5' />
-                <input type='text' id='street' placeholder='Logradouro: Av/Rua/Trav' className='rounded py-0.5' />
-                <input type='text' id='nunresidence' placeholder='Nº Casa/Edifício' className='rounded py-0.5' />
-                <div className='flex gap-5 justify-center p-1'>
-                    <label htmlFor='house' className='flex items-center cursor-pointer'>
-                        <input type='radio' name='residence' id='house' value='house' className='mr-1.5' />
-                        Casa</label>
-                    <label htmlFor='buildings' className='flex items-center cursor-pointer'>
-                        <input type='radio' name='residence' id='buildings' value='building' className='mr-1.5' />
-                        Edifício</label>
+                {title !== 'Entrar' && <div className='flex flex-col gap-[0.313rem]'>
+                    <input type='number' id='zipcode' placeholder='CEP' className='rounded py-0.5' />
+                    <input type='text' id='street' placeholder='Logradouro: Av/Rua/Trav' className='rounded py-0.5' />
+                    <input type='text' id='nunresidence' placeholder='Nº Casa/Edifício' className='rounded py-0.5' />
+                    <div className='flex gap-5 justify-center p-1'>
+                        <label htmlFor='house' className='flex items-center cursor-pointer'>
+                            <input type='radio' name='residence' id='house' value='house' className='mr-1.5' />
+                            Casa</label>
+                        <label htmlFor='buildings' className='flex items-center cursor-pointer'>
+                            <input type='radio' name='residence' id='buildings' value='building' className='mr-1.5' />
+                            Edifício</label>
+                    </div>
+                    <div className='flex flex-col gap-[0.313rem]'>
+                        <input type='text' id='building' placeholder='Nome do Edifício' className='rounded py-0.5' />
+                        <input type='text' id='block' placeholder='Bloco' className='rounded py-0.5' />
+                        <input type='text' id='livingapartmentroom' placeholder='Apartamento/Sala' className='rounded py-0.5' />
+                    </div>
+                    <input type='text' id='neighborhod' placeholder='Bairro/Distrito' className='rounded py-0.5' />
+                    <input type='text' id='city' placeholder='Cidade' className='rounded py-0.5' />
                 </div>
-                <div className='flex flex-col gap-[0.313rem]'>
-                    <input type='text' id='building' placeholder='Nome do Edifício' className='rounded py-0.5' />
-                    <input type='text' id='block' placeholder='Bloco' className='rounded py-0.5' />
-                    <input type='text' id='livingapartmentroom' placeholder='Apartamento/Sala' className='rounded py-0.5' />
-                </div>
-                <input type='text' id='neighborhod' placeholder='Bairro/Distrito' className='rounded py-0.5' />
-                <input type='text' id='city' placeholder='Cidade' className='rounded py-0.5' />
+                }
                 {(title === 'Entrar' || title === 'Cadastrar Usuário') && <div>
                     <input type={ispassword ? 'text' : 'password'} id='password' placeholder='Senha' className='rounded py-0.5' />
                     <button type='button' onClick={handlePassword} className='relative'>
