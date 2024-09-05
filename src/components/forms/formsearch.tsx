41efc4a5194c0d1b65996ba7f11693cd8d4f0b11
Humberto Ribeiro <betofoxmal@gmail.com>
2024-09-04 22:53:41 -0300

    update placeholder color error search

--- src/components/forms/formsearch.tsx
+++ src/components/forms/formsearch.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { FormSearch, InputSearch } from '@/app/types/types';
 import { SubmitHandler, useForm } from 'react-hook-form';
-
 export default function FormSearchs({ search }: FormSearch) {
     const { register, handleSubmit, setError, formState: { errors } } = useForm();
     const onSubmit: SubmitHandler<InputSearch> = (data: any) => {
@@ -11,7 +10,7 @@ export default function FormSearchs({ search }: FormSearch) {
         <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col p-1 w-[280px]'>
             <legend className='mx-auto py-1 duration-[400ms]'>{search}</legend>
             {search === 'Pesquisar Doador' && <div className='flex flex-col'>
-                <input type='search' placeholder='Nome/Telefone/Código' className='rounded py-0.5' {...register('codnametel', { required: true })} />
+                <input type='search' placeholder='Nome/Telefone/Código' className={errors.codnametel ? 'rounded py-0.5 border-red-600 placeholder-red-600' : 'rounded py-0.5'} {...register('codnametel', { required: true })} />
                 <div className='flex'>
                     <input type='submit' value='Código' className='bg-blue-600 text-white font-bold p-2 duration-[400ms] cursor-pointer mx-auto rounded hover:bg-green-600 active:bg-blue-600 active:text-black mt-3' />
                     <input type='submit' value='Nome' className='bg-blue-600 text-white font-bold p-2 duration-[400ms] cursor-pointer mx-auto rounded hover:bg-green-600 active:bg-blue-600 active:text-black mt-3' />
@@ -20,7 +19,7 @@ export default function FormSearchs({ search }: FormSearch) {
             </div>
             }
             {search === 'Código da Doação' && <div className='flex flex-col'>
-                <input type='search' placeholder='Código da Doação' className='rounded py-0.5' {...register('coddonation', { required: true })} />
+                <input type='search' placeholder='Código da Doação' className={errors.coddonation ? 'rounded py-0.5 border-red-600 placeholder-red-600' : 'rounded py-0.5'} {...register('coddonation', { required: true })} />
                 <input type='submit' value='Pesquisar' className='bg-blue-600 text-white font-bold p-2 duration-[400ms] cursor-pointer mx-auto rounded hover:bg-green-600 active:bg-blue-600 active:text-black mt-3' />
             </div>
             }
