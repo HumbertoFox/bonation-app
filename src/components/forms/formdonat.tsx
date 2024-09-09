daec6198985981c323efc72be95f8c327b2f39f7
Humberto Ribeiro <betofoxmal@gmail.com>
2024-09-09 15:59:56 -0300

    update form doação

--- src/components/forms/formdonat.tsx
+++ src/components/forms/formdonat.tsx
@@ -6,78 +6,44 @@ export default function FormDonations({ title, value }: TitleValue) {
     const onSubmit: SubmitHandler<InputsDonations> = async (data: any) => {
         console.log(data);
     };
+    const createInputsDiv0 = (count: number) => (
+        Array.from({ length: count }, (_, index) => (
+            <div className='w-full flex gap-[5px] max-md:flex-col' key={index}>
+                <input type='text' placeholder={`Objeto ${index + 2}`} className='rounded py-0.5' {...register(`objeto${index + 2}`)} />
+                <input type='text' placeholder={'Qant/Caixa/Sacola'} className='w-[150px] rounded py-0.5' {...register(`quant${index + 2}`)} />
+            </div>
+        ))
+    );
+    const createInputsDiv1 = (count: number) => (
+        Array.from({ length: count }, (_, index) => (
+            <div className='w-full flex gap-[5px] max-md:flex-col' key={index}>
+                <input type='text' placeholder={`Objeto ${index + 8}`} className='rounded py-0.5' {...register(`objeto${index + 8}`)} />
+                <input type='text' placeholder={'Qant/Caixa/Sacola'} className='w-[150px] rounded py-0.5' {...register(`quant${index + 8}`)} />
+            </div>
+        ))
+    );
     return (
         <form className='w-[750px] p-1 max-xl:w-[380px] max-md:w-[280px]' onSubmit={handleSubmit(onSubmit)}>
             <fieldset className='flex flex-col gap-[5px]'>
                 <legend className='mx-auto py-1 duration-[400ms]'>Lista de objetos a serem doados</legend>
                 <div className='flex gap-[5px] max-xl:flex-col'>
                     <label htmlFor='coddonation' className='flex flex-col'>Código da Doação
-                        <input type='text' id='coddonation' disabled placeholder='Código da Doação' className='rounded py-0.5 cursor-not-allowed' {...register('coddonation')} />
+                        <input type='text' id='coddonation' disabled placeholder={errors.coddonation ? 'Campo Obrigatório' : 'Código da Doação'} className={errors.coddonation ? 'rounded py-0.5 border-red-600 placeholder-red-600' : 'rounded py-0.5 cursor-not-allowed'} {...register('coddonation', { required: true })} />
                     </label>
                     <label htmlFor='donorcode' className='flex flex-col'>Código do Doador
-                        <input type='text' id='donorcode' placeholder='Código do Doador' disabled className='rounded py-0.5 cursor-not-allowed' />
+                        <input type='text' id='donorcode' placeholder={errors.donorcode ? 'Campo Obrigatório' : 'Código do Doador'} disabled className={errors.donorcode ? 'rounded py-0.5 border-red-600 placeholder-red-600' : 'rounded py-0.5 cursor-not-allowed'} {...register('donorcode', { required: true })} />
                     </label>
                 </div>
                 <div className='flex gap-[5px] duration-[400ms] max-xl:flex-wrap'>
                     <div className='w-full flex flex-col gap-[5px] duration-[400ms]'>
                         <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto1' placeholder='Objeto 1' className='rounded py-0.5' />
-                            <input type='text' id='quant1' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
+                            <input type='text' id='objeto1' placeholder={errors.objeto1 ? 'Campo Obrigatório' : 'Objeto 1'} className={errors.objeto1 ? 'rounded py-0.5 border-red-600 placeholder-red-600' : 'rounded py-0.5'} {...register('objeto1', { required: true })} />
+                            <input type='text' id='quant1' placeholder={errors.quant1 ? 'Campo Obrigatório' : 'Qant/Caixa/Sacola'} className={errors.quant1 ? 'w-[150px] rounded py-0.5 border-red-600 placeholder-red-600' : 'w-[150px] rounded py-0.5'} {...register('quant1', { required: true })} />
                         </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto2' placeholder='Objeto 2' className='rounded py-0.5' />
-                            <input type='text' id='quant2' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto3' placeholder='Objeto 3' className='rounded py-0.5' />
-                            <input type='text' id='quant3' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto4' placeholder='Objeto 4' className='rounded py-0.5' />
-                            <input type='text' id='quant4' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto5' placeholder='Objeto 5' className='rounded py-0.5' />
-                            <input type='text' id='quant5' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto6' placeholder='Objeto 6' className='rounded py-0.5' />
-                            <input type='text' id='quant6' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto7' placeholder='Objeto 7' className='rounded py-0.5' />
-                            <input type='text' id='quant7' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
+                        {createInputsDiv0(6)}
                     </div>
                     <div className='w-full flex flex-col gap-[5px] duration-[400ms]'>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto8' placeholder='Objeto 8' className='rounded py-0.5' />
-                            <input type='text' id='quant8' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto9' placeholder='Objeto 9' className='rounded py-0.5' />
-                            <input type='text' id='quant9' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto10' placeholder='Objeto 10' className='rounded py-0.5' />
-                            <input type='text' id='quant10' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto11' placeholder='Objeto 11' className='rounded py-0.5' />
-                            <input type='text' id='quant11' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto12' placeholder='Objeto 12' className='rounded py-0.5' />
-                            <input type='text' id='quant12' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto13' placeholder='Objeto 13' className='rounded py-0.5' />
-                            <input type='text' id='quant13' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
-                        <div className='w-full flex gap-[5px] max-md:flex-col'>
-                            <input type='text' id='objeto14' placeholder='Objeto 14' className='rounded py-0.5' />
-                            <input type='text' id='quant14' placeholder='Qant/Caixa/Sacola' className='w-[150px] rounded py-0.5' />
-                        </div>
+                        {createInputsDiv1(7)}
                     </div>
                 </div>
                 <textarea id='obs' placeholder='Observações' className='rounded py-0.5 h-20' />
